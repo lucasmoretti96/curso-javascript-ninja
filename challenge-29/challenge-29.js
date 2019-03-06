48bdb6ae6700944e0cbaa0b2f11e4820b6c80247
Lucas Pereira Moretti <lucas.moretti@easynvest.com.br>
2019-03-06 15:41:19 -0300

    Challenge-29 Não terminado

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,4 +1,4 @@
-(function() {
+(function(win, doc) {
   'use strict';
 
   /*
@@ -36,4 +36,36 @@
   que será nomeado de "app".
   */
 
-})();
+  function app() {
+    return {
+      init: function() {
+        this.companyInfo();
+      },
+
+      companyInfo: function companyInfo() {
+        var ajax = new XMLHttpRequest();
+        ajax.open('GET', 'company.json', true);
+        ajax.send();
+        ajax.addEventListener('onreadystatechange', this.getCompanyInfo, false);
+      },
+
+      getCompanyInfo: function getCompanyInfo() {
+        if(!app().isReady.call(this))
+          return;
+
+        var data = JSON.parse(this.responseText);
+        var $companyName = new DOM('[data-js="company-name"]');
+        var $companyPhone = new DOM('[data-js="company-phone"]');
+        $companyName.get()[0].textContent = data.name;
+        $companyPhone.get()[0].textContent = data.phone;
+      },
+
+      isReady: function isReady(){
+        return this.readyState === 4 && this.status === 200;
+      }
+    };
+  }
+
+  app().init();
+
+})(window, document);
